75948a74d343b688db25b131d05c2a0a1cc01a82
Timo Jarventausta <timo.jaerv@gmail.com>
2015-08-09 22:28:06 +0300

    Added the option framework

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,10 @@
 from flask.ext.wtf import Form
-from wtforms import StringField
+from wtforms import StringField, IntegerField
+from flask.ext.wtf.html5 import RangeInput
+
 
 class TestForm(Form):
-    test = StringField('testing')
+    performance = IntegerField('Performance', widget=RangeInput())
+    screen = IntegerField('Screen', widget=RangeInput())
+    mobility = IntegerField('Mobility', widget=RangeInput())
+    battery = IntegerField('Battery', widget=RangeInput())
--- app/views.py
+++ app/views.py
@@ -1,10 +1,33 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for, session
 from .forms import TestForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-	form = TestForm()
-	return render_template('index.html', form = form)
+	if 'budget' in session:
+		return redirect(url_for('options'))
+	return redirect(url_for('budget'))
+
+@app.route('/budget', methods=['GET', 'POST'])
+def budget():
+	try:
+		session['budget'] = request.form['budget']
+	except KeyError:
+		return render_template('budget.html')
+	return redirect(url_for('options'))
+
+@app.route('/options')
+def options():
+	if 'budget' in session:
+		form = TestForm()
+		for field in form:
+			print field.id
+		return render_template('options.html', form=form, budget=session['budget'])
+	return redirect(url_for('budget'))
+
+'''@app.route('/post', methods=['GET', 'POST'])
+def post():
+	return request.form['budget']
+'''
