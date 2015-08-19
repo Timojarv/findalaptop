5fc9c52663eb8983b9c916b71ea8571cea191e80
Timo Jarventausta <timo.jaerv@gmail.com>
2015-08-19 18:27:01 +0300

    UI tweaks

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from flask.ext.wtf.html5 import RangeInput
 
 class TestForm(Form):
     performance = IntegerField('Performance', widget=RangeInput())
-    screen = IntegerField('Screen', widget=RangeInput())
+    screen = IntegerField('Screen quality', widget=RangeInput())
+    sound = IntegerField('Sound quality', widget=RangeInput())
     mobility = IntegerField('Mobility', widget=RangeInput())
     battery = IntegerField('Battery', widget=RangeInput())
-    sound = IntegerField('Sound quality', widget=RangeInput())
--- app/views.py
+++ app/views.py
@@ -6,26 +6,31 @@ from .forms import TestForm
 @app.route('/')
 @app.route('/index')
 def index():
-	if 'budget' in session:
-		return redirect(url_for('options'))
-	return redirect(url_for('budget'))
+    if 'budget' in session:
+        return redirect(url_for('options'))
+    return redirect(url_for('budget'))
+
 
 @app.route('/budget', methods=['GET', 'POST'])
 def budget():
-	try:
-		session['budget'] = request.form['budget']
-	except KeyError:
-		return render_template('budget.html')
-	return redirect(url_for('options'))
+    try:
+        session['budget'] = request.form['budget']
+    except KeyError:
+        return render_template('budget.html')
+    return redirect(url_for('options'))
 
-@app.route('/options')
+
+@app.route('/options', methods=['GET', 'POST'])
 def options():
-	if 'budget' in session:
-		form = TestForm()
-		for field in form:
-			print field.id
-		return render_template('options.html', form=form, budget=session['budget'])
-	return redirect(url_for('budget'))
+    if 'budget' in session:
+        form = TestForm()
+        if form.validate_on_submit():
+            data = session['budget'] + "e, "
+            for field in form:
+                data += str(field.data) + "<br>"
+            return data
+        return render_template('options.html', form=form, budget=session['budget'])
+    return redirect(url_for('budget'))
 
 '''@app.route('/post', methods=['GET', 'POST'])
 def post():
